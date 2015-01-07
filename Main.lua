edcb17d246fbd2f4f4f908410193313eb3b0a7e9
seba1 <sebahoroszko@gmail.com>
2015-01-07 15:42:59 +0000

    added coroutines

--- Main.lua
+++ Main.lua
@@ -10,10 +10,11 @@ function OBJ_A:updateFromA(p)
 	pxT ={p}
 	sendMsg(self, myOBJ_B, "updateFromB", pxT)
 end
-function OBJ_A:Main()
---loop until not empty BUT firstly do threads
---while ... do
-	execMsg(getMsg(self))
+function OBJ_A:Main(threadID)
+	while true do
+		execMsg(getMsg(self))
+		coroutine.yield(thread[threadID])
+	end
 end
 ------------------------ OBJ_B OBJ -------------------------
 OBJ_B = oo.class {
@@ -24,40 +25,66 @@ function OBJ_B:updateFromB(a)
 	axT ={a}
 	sendMsg(self, myOBJ_A, "updateFromA", axT)
 end
-function OBJ_B:Main()
---loop until not empty BUT firstly do threads
---while ... do
-	execMsg(getMsg(self))
+function OBJ_B:Main(threadID)
+	while true do
+		execMsg(getMsg(self))
+		coroutine.yield(thread[threadID])
+	end
 end
+
 --------------------------- MAIN ---------------------------
-print()
---create two objects
-myOBJ_A = OBJ_A {
-	obj_A_value=240,
-}
-myOBJ_B = OBJ_B {
-	obj_B_value=240,
-}
+function Main()
+	print()
+	myOBJ_A = OBJ_A {
+		obj_A_value=240,
+	}
+	myOBJ_B = OBJ_B {
+		obj_B_value=240,
+	}
+	local x=5
+	myOBJ_A:updateFromA(x)
+	myOBJ_B:updateFromB(x)
+	
+	print ("\nFrom Main:")
+	objList={myOBJ_A,myOBJ_B}
+	print("OBJ_A:   ",myOBJ_A)
+	print("OBJ_B:   ",myOBJ_B)
+	print("objList: ",objList)
+	
+	
+	thread ={}
+	thread['0'] = coroutine.create(function()
+		while true do
+			--for each_obj do
+			local i=0
+			for _, value in pairs(objList) do 
+				i=i+1
+				local c=tostring(i)
+				thread[c] = coroutine.create(function()
+					value:Main(c)
+				end)
+				coroutine.resume(thread[c])
+				
+			end
+			print(myOBJ_A.obj_A_value)
+			print(myOBJ_B.obj_B_value,"\n")
+			os.execute("ping 1.1.1.1 -n 1 -w 1000 > nul") -- wait 1 sec
+		end
+	end)
+	coroutine.resume(thread['0'])
 
-local x=5
-myOBJ_A:updateFromA(x)
---mailbox after line above: {{myOBJ_A, myOBJ_B, "updateFromB", {x}}}
-myOBJ_B:updateFromB(x)
---mailbox after line above:
---[[ 				Mailbox
-	{{myOBJ_A, myOBJ_B, "updateFromB", {x}},
-	 {myOBJ_B, myOBJ_A, "updateFromA", {x}}}	 
---]]
+end	--Main
+Main()
 
---TODO add Thread for while
-while true do
---TODO for 'each obj' do
---TODO add Thread
-	myOBJ_A:Main()
-	myOBJ_B:Main()
-	print ("OBJ A Value: "..myOBJ_A.obj_A_value)
-	print ("OBJ B Value: "..myOBJ_B.obj_B_value.."\n")
-	os.execute("ping 1.1.1.1 -n 1 -w 1000 > nul") -- wait 1 sec
-end
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 ------------------------- END OF FILE ----------------------
